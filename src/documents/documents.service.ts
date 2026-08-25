5b9e0830ff4f7b76512751931d85efc8abb7f5fb
Tom Eckstein <t.eckstein98@gmail.com>
2026-08-25 21:04:09 +0200

    Add file rading and chunking logic to documents service

--- src/documents/documents.service.ts
+++ src/documents/documents.service.ts
@@ -1,4 +1,24 @@
 import { Injectable } from '@nestjs/common';
+import { readFile } from 'fs/promises';
 
 @Injectable()
-export class DocumentsService {}
+export class DocumentsService {
+  private async getFileText(filePath: string): Promise<string> {
+    const fileText = await readFile(filePath, { encoding: 'utf8' });
+    return fileText;
+  }
+
+  private chunkText(text: string, chunkSize: number = 500): string[] {
+    const numOfChunks = Math.ceil(text.length / chunkSize);
+    const textChunks: string[] = [];
+    for (let i = 0; i < numOfChunks; i++) {
+      textChunks.push(text.slice(i * chunkSize, (i + 1) * chunkSize));
+    }
+    return textChunks;
+  }
+
+  public async readAndChunkFile(filePath: string): Promise<string[]> {
+    const textOfFile = await this.getFileText(filePath);
+    return this.chunkText(textOfFile);
+  }
+}
