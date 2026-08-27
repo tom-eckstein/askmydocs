b6d6cdf0b00c6a363c3f247f40913792e00be180
Tom Eckstein <t.eckstein98@gmail.com>
2026-08-27 20:47:05 +0200

    Add JSDoc documentation to documents service methods

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DocumentsModule } from './documents/documents.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [DocumentsModule],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), DocumentsModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/documents/documents.controller.ts
+++ src/documents/documents.controller.ts
@@ -1,4 +1,20 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { DocumentsService } from './documents.service';
+import { ConfigService } from '@nestjs/config';
 
 @Controller('documents')
-export class DocumentsController {}
+export class DocumentsController {
+  constructor(
+    private readonly documentService: DocumentsService,
+    private readonly configService: ConfigService,
+  ) {}
+
+  @Get()
+  async getFileText(): Promise<string[]> {
+    const filePath = this.configService.get<string>('Test_FILE_PATH');
+    if (!filePath) {
+      throw new Error('TEST_FILE_PATH is not defined in environment variables');
+    }
+    return await this.documentService.readAndChunkFile(filePath);
+  }
+}
