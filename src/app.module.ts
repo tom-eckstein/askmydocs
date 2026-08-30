0cf30775d5a9993494ec8b94bc93b928b7e9f50e
Tom Eckstein <t.eckstein98@gmail.com>
2026-08-30 17:17:44 +0200

    Add zod dependency and register Ollama module in app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DocumentsModule } from './documents/documents.module';
 import { ConfigModule } from '@nestjs/config';
+import { OllamaModule } from './ollama/ollama.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), DocumentsModule],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), DocumentsModule, OllamaModule],
   controllers: [AppController],
   providers: [AppService],
 })
