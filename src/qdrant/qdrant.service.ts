66408d7af78427d836848b17dadbae44dfb91786
Tom Eckstein <t.eckstein98@gmail.com>
2026-08-30 17:42:15 +0200

    Add Qdrant service with client initialization

--- src/qdrant/qdrant.service.ts
+++ src/qdrant/qdrant.service.ts
@@ -1,4 +1,18 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { QdrantClient } from '@qdrant/js-client-rest';
 
 @Injectable()
-export class QdrantService {}
+export class QdrantService {
+  private readonly client: QdrantClient;
+  private readonly logger = new Logger(QdrantService.name);
+
+  constructor(private readonly configService: ConfigService) {
+    const qdrantUrl = this.configService.get<string>('QDRANT_URL');
+    if (!qdrantUrl) {
+      this.logger.fatal('QDRANT_URL is not defined in environment variables');
+      throw new Error('QDRANT_URL is not defined in environment variables');
+    }
+    this.client = new QdrantClient({ url: qdrantUrl });
+  }
+}
