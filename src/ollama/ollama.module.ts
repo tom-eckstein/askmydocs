dc746224b55ab8b8486ea7f0c28e6d5633301b47
Tom Eckstein <t.eckstein98@gmail.com>
2026-09-01 18:32:27 +0200

    Remove unused debug controllers for Ollama, Qdrant and Documents

--- src/ollama/ollama.module.ts
+++ src/ollama/ollama.module.ts
@@ -1,9 +1,7 @@
 import { Module } from '@nestjs/common';
 import { OllamaService } from './ollama.service';
-import { OllamaController } from './ollama.controller';
 
 @Module({
   providers: [OllamaService],
-  controllers: [OllamaController]
 })
 export class OllamaModule {}
--- src/qdrant/qdrant.module.ts
+++ src/qdrant/qdrant.module.ts
@@ -1,9 +1,7 @@
 import { Module } from '@nestjs/common';
 import { QdrantService } from './qdrant.service';
-import { QdrantController } from './qdrant.controller';
 
 @Module({
   providers: [QdrantService],
-  controllers: [QdrantController]
 })
 export class QdrantModule {}
