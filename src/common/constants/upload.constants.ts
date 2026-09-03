94e16a3beebd639fc278b530fe241db442d51e4f
Tom Eckstein <t.eckstein98@gmail.com>
2026-09-03 13:29:32 +0200

    Add file size and count limits for uploads

---
+++ src/common/constants/upload.constants.ts
@@ -0,0 +1,2 @@
+export const MAX_FILE_SIZE_BYTES = 20 * 1024 * 1024; // 20MB
+export const MAX_FILES_PER_UPLOAD = 10;
--- src/ingestion/ingestion.controller.ts
+++ src/ingestion/ingestion.controller.ts
@@ -8,7 +8,11 @@ import {
 import { IngestionService } from './ingestion.service';
 import { ConfigService } from '@nestjs/config';
 import { FilesInterceptor } from '@nestjs/platform-express';
-import { UploadFilesDto } from './dto/upload-files.dto';
+import { UploadFilesDTO } from './dto/upload-files.dto';
+import {
+  MAX_FILE_SIZE_BYTES,
+  MAX_FILES_PER_UPLOAD,
+} from 'src/common/constants/upload.constants';
 
 @Controller('ingestion')
 export class IngestionController {
@@ -18,10 +22,14 @@ export class IngestionController {
   ) {}
 
   @Post('/upload')
-  @UseInterceptors(FilesInterceptor('files'))
+  @UseInterceptors(
+    FilesInterceptor('files', MAX_FILES_PER_UPLOAD, {
+      limits: { fileSize: MAX_FILE_SIZE_BYTES },
+    }),
+  )
   ingestFiles(
     @UploadedFiles() files: Express.Multer.File[],
-    @Body() dto: UploadFilesDto,
+    @Body() dto: UploadFilesDTO,
   ) {
     return this.ingestionsService.ingestFiles(files, dto.includesHeaders);
   }
